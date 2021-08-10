e8feef670f8a4feb639f9d868fb08c8a3bc15030
3predu <3pedrolucca@gmail.com>
2021-08-10 14:04:42 -0300

    Adicionando aula09

---
+++ aula09/ex005.js
@@ -0,0 +1,12 @@
+var corpo = window.document.body;
+var p1 = window.document.getElementsByTagName('p')[1];
+    /*
+    d.style.background = 'red'
+    d.innerText = 'Estou aguardando...'
+    */
+
+    /*var d = window.document.getElementsByTagName('msg')[0]
+    d.innerText = 'Hello!' 
+    */
+var d = window.document.querySelector('div#msg'); // método recente, recomendado!!
+d.style.color = 'red';
